db6a3b30d6c6dfade80f5b6d39d202a616074384
Juraj Paluba <juraj.palubajp@gmail.com>
2026-08-04 21:16:33 +0200

    docs(server): create module and function documentation
    ---v2
    Fix review comments

--- hivex_proxy_server/lib/hivex_proxy_server/listener_handler.ex
+++ hivex_proxy_server/lib/hivex_proxy_server/listener_handler.ex
@@ -1,12 +1,17 @@
 defmodule HivexProxyServer.ListenerHandler do
   @moduledoc """
-  TODO
+  The handler of connections for bind listeners.
+
+  This handler has a reference to the associated tunnel in the `state` under `:client_socket` key.
   """
 
   use ThousandIsland.Handler
 
   require Logger
 
+  @doc """
+  Handle all incoming data and forward the data to the correct tunnel -- `:client_socket`.
+  """
   @impl ThousandIsland.Handler
   def handle_data(data, _socket, state) do
     Logger.info(message: "Listener got data", data: data)
