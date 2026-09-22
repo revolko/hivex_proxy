a536ca7e9e53ab8f2eb9083a3234d1433d130d9e
Juraj Paluba <juraj.palubajp@gmail.com>
2026-09-22 22:02:33 +0200

    refactor(server): move all handle_data to tunnel
    The main change is the simplification of the bind request error
    handling. The reason for removing most of the error checks is the
    "fail-fast" approach. Basically, if any of the bind request step cannot
    be done, the connection is "beyond saving" -- e.g. requested listening
    port is taken or ThousandIslands cannot start another listener. There is
    not way to fix the error and so there is no difference between handling
    error and closing the connection and not handling error, failing the
    handler and letting the supervisor of the handler to handle the error.
    
    Remove useless closing of the listener -- beam automatically kills all
    children (listener) when the parent (handler) is killed. So we don't
    need to handle that explicitly.

--- hivex_proxy_server/lib/hivex_proxy_server/tunnel.ex
+++ hivex_proxy_server/lib/hivex_proxy_server/tunnel.ex
@@ -5,11 +5,80 @@ defmodule HivexProxyServer.Tunnel do
 
   require Logger
 
+  @version 0x1
+  @bind_command 0x1
+  @health_check_signal <<0::6*8>>
+  @sender_ref_length 32
+  @error_status_length 8
+
   defstruct listening: false,
             listener_pid: nil
 
   def init() do
     %__MODULE__{}
+  end
+
+  # BIND frame
+  def handle_frame(
+        <<@version, @bind_command, port::binary-size(2)>>,
+        socket,
+        %__MODULE__{listening: false} = tunnel
+      ) do
+    Logger.debug(message: "Handling bind request", port: port)
+
+    with {:ok, pid} <-
+           ThousandIsland.start_link(
+             port: :binary.decode_unsigned(port),
+             handler_module: HivexProxyServer.ListenerHandler,
+             handler_options: [bind_handler: self()]
+           ),
+         {:ok, {_listen_address, listen_port}} <- ThousandIsland.listener_info(pid),
+         :ok <- ThousandIsland.Socket.send(socket, <<@version, @bind_command, listen_port::16>>) do
+      {:ok, %{tunnel | listening: true, listener_pid: pid}}
+    else
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  # RESPONSE frame
+  def handle_frame(
+        <<sender_length::@sender_ref_length, sender_ref::binary-size(sender_length),
+          error_status::@error_status_length, data::binary>>,
+        _socket,
+        %__MODULE__{listening: true} = tunnel
+      ) do
+    Logger.debug(
+      message: "Forwarding client proxy data to the listener",
+      error_status: error_status,
+      data: data
+    )
+
+    listener_from = :erlang.binary_to_term(sender_ref)
+
+    case error_status do
+      0 ->
+        GenServer.reply(listener_from, {:ok, data})
+
+      _ ->
+        GenServer.reply(listener_from, {:error, data})
+    end
+
+    {:ok, tunnel}
+  end
+
+  # HEALTH-CHECK frame
+  def handle_frame(<<@version>> <> @health_check_signal, socket, tunnel) do
+    Logger.debug(message: "Received health check")
+
+    case ThousandIsland.Socket.send(socket, <<@version>> <> @health_check_signal) do
+      :ok ->
+        Logger.debug(message: "Health check ACK sent")
+
+      {:error, error} ->
+        Logger.warning(message: "Failed to send health check ACK", details: error)
+    end
+
+    {:ok, tunnel}
   end
 
   def handle_frame(
