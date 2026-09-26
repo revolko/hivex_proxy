395ab157bed5e57171febd1769b1ebd941742b98
Juraj Paluba <juraj.palubajp@gmail.com>
2026-09-26 19:30:45 +0200

    feat(client): create client start_link
    The function can be used in other applications to start the proxy client
    under a single application supervisor.

--- hivex_proxy_client/lib/hivex_proxy_client.ex
+++ hivex_proxy_client/lib/hivex_proxy_client.ex
@@ -2,4 +2,23 @@ defmodule HivexProxyClient do
   @moduledoc """
   TODO
   """
+
+  def child_spec(opts) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, [opts]},
+      type: :supervisor,
+      restart: :permanent
+    }
+  end
+
+  def start_link(_) do
+    children = [
+      {HivexProxyClient.ConnectionsSupervisor, []},
+      {Task.Supervisor, name: HivexProxyClient.ServerConnectionsSupervisor}
+    ]
+
+    opts = [strategy: :one_for_one, name: HivexProxyClient.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
 end
